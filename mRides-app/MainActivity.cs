bcbbed87c9857b89c6de8ea9a3a7503cc67674d6
nim442 <nim442@gmail.com>
2017-01-21 11:16:35 -0500

    Add project files.

---
+++ mRides-app/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace mRides_app
+{
+    [Activity(Label = "mRides_app", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
