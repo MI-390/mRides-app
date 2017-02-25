917f2eae45e04e209a768a1fe9b22bb0ab9380ac
Aline Koftikian <aline.koftikian@gmail.com>
2017-02-25 03:37:53 -0500

    MI-115 Displaying user profile fragment code

--- mRides-app/UserProfileFeedbackFragment.cs
+++ mRides-app/UserProfileFeedbackFragment.cs
@@ -15,7 +15,37 @@ namespace mRides_app
 {
     class UserProfileFeedbackFragment : ListFragment
     {
+        TextView dateTV;
+        TextView nameTV;
+        Textview reviewTV;
+        RatingBar ratingBar;
 
+        // Static method to create a new instance of this fragment
+        public static UserProfileFeedbackFragment NewInstance(Bundle bundle)
+        {
+            UserProfileFeedbackFragment fragment = new UserProfileFeedbackFragment();
+            fragment.Arguments = bundle;
+            return fragment;
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+            base.OnCreateView(inflater, container, savedInstanceState);
+            View view = inflater.Inflate(Resource.Layout.UserProfileFeedbackFragment, container, false);
+            dateTV = view.FindViewById<TextView>(Resource.Id.feedbackDateProfileFragment);
+            nameTV = view.FindViewById<TextView>(Resource.Id.feedbackFragmentUserName);
+            reviewTV = view.FindViewById<TextView>(Resource.Id.feedbackFragmentReview);
+            ratingBar = view.FindViewById<TextView>(Resource.Id.feedbackFragmentRatingBar);
+
+            return view;
+        }
+
+        public override void OnAttach(Activity activity)
+        {
+            base.OnAttach(activity);
+        }
 
     }
 }
