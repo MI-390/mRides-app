919ab6d21c14e9f1a339ccbf4f478877d8f6a25e
limphilip <lim-philip@hotmail.com>
2017-02-26 21:20:26 -0500

    Fix: Removed the creation of empty user

--- mRides-app/MapActivity.cs
+++ mRides-app/MapActivity.cs
@@ -230,8 +230,7 @@ namespace mRides_app
             {
                 destinationCoordinates.Add(directionList[i].Latitude.ToString() + "," + directionList[i].Longitude.ToString());
             }
-
-            User.currentUser = UserMapper.getInstance().CreateUser(new User());
+            
             Request newRequest = new Request {
                 destinationCoordinates = destinationCoordinates,
                 destination= destinationCoordinates[destinationCoordinates.Count-1],
