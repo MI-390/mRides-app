2e2894bc13492475ba67fba73e12aca3fea07068
hannah-hp\Hannah <lortiz.hannah@gmail.com>
2017-03-15 00:19:29 -0400

    #MI175 implemented the UI for when a Driver gets match with a Rider. Also included the option of viewing the current time of the pickup.

--- mRides-app/DriverAcceptDeclineMatchActivity.cs
+++ mRides-app/DriverAcceptDeclineMatchActivity.cs
@@ -12,40 +12,41 @@ using Android.Widget;
 
 namespace mRides_app
 {
-    [Activity(Label = "DriverAcceptDeclineMatchActivity")]
+    [Activity(Label ="DriverAcceptDeclineMatchActivity")]
     public class DriverAcceptDeclineMatchActivity : Activity
     {
-   
-        //declaring variable for the layout elements and time fields.
-        private TextView time_display;
+
+        private TextView show_time;
         private int hour;
         private int minute;
-
-        const int TIME_DIALOG_ID = 0;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "Main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.DriverAcceptDeclineMatch);
 
-            // Capture our View elements
-            time_display = FindViewById<TextView>(Resource.Id.timeDisplay);
-            
-
-            // Add a click listener to the button
-           
+            // Capture View elements
+            TextView show_time = FindViewById<TextView>(Resource.Id.displayTime);
+                      
 
             // Get the current time
             hour = DateTime.Now.Hour;
             minute = DateTime.Now.Minute;
 
             // Display the current date
-           
+            UpdateDisplay();
         }
 
-    
+        // Updates the time we display in the TextView
+        private void UpdateDisplay()
+        {
+            string time = string.Format("{0}:{1}", hour, minute.ToString().PadLeft(2, '0'));
+            show_time.Text = time;
+        }
+
+
+
 
     }
 }
