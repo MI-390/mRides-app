6a7f3a092c651777ae1cc82db11b62a5b2088c62
Ideawin-Bunthy Koun <ideawinkoun@gmail.com>
2017-04-04 20:55:12 -0400

    #82 Added an if/else statement inside UserProfileActivity to change the background color to red if a driver and green if a rider. Fixed LeaveReviewFragment color which showed the opposite color of what was wanted

--- mRides-app/Activities/UserProfileActivity.cs
+++ mRides-app/Activities/UserProfileActivity.cs
@@ -65,6 +65,21 @@ namespace mRides_app
 
             }
 
+            LinearLayout layout = FindViewById<LinearLayout>(Resource.Id.profileBackground);
+
+            // Set background color to the right color
+            if (User.currentUser != null)
+            {
+                if (User.currentUser.currentType == "rider")
+                {
+                    layout.SetBackgroundColor(new Android.Graphics.Color(Color.ParseColor("#26A65B")));
+                }
+                else
+                {
+                    layout.SetBackgroundColor(new Android.Graphics.Color(Color.ParseColor("#EF5350")));
+                }
+            }
+
             var userProfileFeedbackAdapter = new UserProfileFeedbackAdapter(this, userFeedback);
             var feedbackListView = FindViewById<ListView>(Resource.Id.userProfileListView);
             feedbackListView.Adapter = userProfileFeedbackAdapter;
